40b7994afa0565d19bddeb7c424dfefecad12fe1
Ben Math <benjamin.aaron.math@gmail.com>
2021-12-20 15:38:50 -0500

    hello world

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-rcuval91t&13v(zxo^%gk^p9lm)ww@(*h@g*j23p%f!d5jqotx'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
